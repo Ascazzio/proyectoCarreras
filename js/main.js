d1ce57fe55ad7e837354a47fc1c7dbe5cb96cfa4
Fabrizio Pironi <fabriziopironi@gmail.com>
2021-10-01 12:46:35 +0200

    Mejora de trapecios

--- js/main.js
+++ js/main.js
@@ -1,10 +1,13 @@
-screenChanger = (fase) => {
-    let arrFases = ["1", "2", "3"];
+// screenChanger = (fase) => {
+//     let arrFases = ["1", "2", "3"];
     
-    arrFases = arrFases.filter(val => !fase.includes(val));
-    document.getElementById(fase).style.display = "block";
+//     arrFases = arrFases.filter(val => !fase.includes(val));
+//     document.getElementById(fase).style.display = "block";
 
-    for (let f of arrFases) {
-        document.getElementById(f).style.display = "none";
-    }
-}
+//     for (let f of arrFases) {
+//         document.getElementById(f).style.display = "none";
+//     }
+// }
+
+tecla = prompt(KeyboardEvent.keyCode);
+console.log(tecla);
