d6a224aecfead163cabb823bbefdfd8de3c85fd6
Fabrizio Pironi <fabriziopironi@gmail.com>
2021-10-04 22:52:04 +0200

    Movimiento listo
    El jugador es capaz de ir de izquierda a derecha, las imágenes de todos los coches ha sido añadido

--- js/juego.js
+++ js/juego.js
@@ -22,8 +22,72 @@ const movCarretera = () => {
         }
         
 
-    }, 100);
+    }, 500);
 
+}
+
+//   Movimiento
+
+let carril = 2;
+
+const keyEvent = (event) => {
+
+    
+    switch(event.keyCode){
+        case 32:
+            console.log("pulsaste la barra espaciadora");
+            
+        break;
+
+        //Derecha
+        case 39:
+            console.log("pulsaste la flecha hacia derecha");
+            
+            if (document.getElementById("jugador").classList.contains("izq")) {
+                document.getElementById("jugador").classList.remove("izq");
+                document.getElementById("jugador").src = "../img/challenger.png";
+            } else {
+                document.getElementById("jugador").src = "../img/challengerIzq.png";
+                document.getElementById("jugador").classList.add("der");
+            }
+
+            setTimeout(() => {
+                carril = carril + 1;    
+            }, 250);
+
+            console.log(carril);
+
+            
+        break;
+
+        //Izquierda
+        case 37:
+            console.log("pulsaste la flecha hacia izquierda");
+            document.getElementById("jugador").src = "../img/challengerDer.png";
+
+            if (document.getElementById("jugador").classList.contains("der")) {
+                document.getElementById("jugador").classList.remove("der");
+                document.getElementById("jugador").src = "../img/challenger.png";
+            } else {
+                document.getElementById("jugador").src = "../img/challengerDer.png";
+                document.getElementById("jugador").classList.add("izq");
+            }
+
+            setTimeout(() => {
+                carril = carril - 1;    
+            }, 250);
+
+            console.log(carril);
+
+
+        break;
+
+        default:
+        
+        break;
+    }
+
+   
 }
 
 movCarretera();
